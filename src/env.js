5026679db33d75efec07f8dbfef23f9d2f9512d6
Lucas Gomes <lucasdeoliveira887@gmail.com>
2020-09-27 19:36:32 -0300

    Added dotenv config

---
+++ src/env.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  HOST: process.env.HOST || 'localhost',
+  PROTOCOL: process.env.PROTOCOL || 'http',
+  PORT: process.env.PORT || 3000,
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost:27017/seqtdb',
+  LOGGER_LEVEL: process.env.LOGGER_LEVEL || 'debug',
+  PIPEDRIVE_API_TOKEN: process.env.PIPEDRIVE_API_TOKEN,
+  BLING_HOST: process.env.BLING_HOST,
+  BLING_API_KEY: process.env.BLING_API_KEY
+}
