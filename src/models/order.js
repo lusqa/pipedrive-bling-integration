3f38ede44e30a8c52c1a8ffb76c05cc55d2669a0
Lucas Gomes <lucasdeoliveira887@gmail.com>
2020-09-27 23:44:24 -0300

    Added order model to store the deals from pipedrive

---
+++ src/models/order.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const schema = new Schema(
+  {
+    id_deal: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    client: {
+      company_name: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      }
+    },
+    product: {
+      title: {
+        type: String,
+        required: true
+      },
+      currency: {
+        type: String,
+        required: true
+      },
+      value: {
+        type: Number,
+        required: true
+      }
+    }
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  }
+)
+
+schema.statics.createOrder = async function (params) {
+  const session = await mongoose.startSession()
+
+  await session.withTransaction(async () => {
+    const existingOrder = await this.findOne({ id_deal: params.id_deal })
+
+    if (!existingOrder) {
+      await this.create({
+        id_deal: params.id_deal,
+        client: {
+          company_name: params.company_name,
+          name: params.name
+        },
+        product: {
+          title: params.title,
+          currency: params.currency,
+          value: params.value
+        }
+      })
+    }
+  })
+
+  session.endSession()
+}
+
+module.exports = mongoose.model('order', schema, 'order')
