ca78bd32bcb9336c839287ad8bbd5630b31cd996
Lucas Gomes <lucasdeoliveira887@gmail.com>
2020-09-28 00:46:27 -0300

    Added swagger endpoint

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,10 +1,14 @@
 const { Router } = require('express')
+const swaggerUi = require('swagger-ui-express')
+
 const order = require('./order.routes')
+const swaggerDocument = require('../../swagger.json')
 
 module.exports = () => {
   const routes = Router()
 
   routes.use('/order', order())
+  routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
   return routes
 }
