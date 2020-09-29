63cc9016abaf1443f46894402ffda46def38c96a
Lucas Gomes <lucasdeoliveira887@gmail.com>
2020-09-29 01:15:20 -0300

    Try to fix func tests on travis

--- test/functional/config/setup.js
+++ test/functional/config/setup.js
@@ -14,5 +14,9 @@ beforeEach(function (done) {
 })
 
 afterEach(function (done) {
-  mongoose.connection.db.dropDatabase(done)
+  if (mongoose.connection.db) {
+    mongoose.connection.db.dropDatabase(done)
+  } else {
+    done()
+  }
 })
